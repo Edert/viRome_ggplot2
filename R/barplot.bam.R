365d07c82580c2307e1871591f4db86fd1afd677
Thomas Eder <thomas.eder@vetmeduni.ac.at>
2022-08-25 08:56:39 +0200

    barplot.bam modified for ggplot2

--- R/barplot.bam.R
+++ R/barplot.bam.R
@@ -1,65 +1,25 @@
-barplot.bam <- function(vdf=NULL, minlen=1, maxlen=37, poscol="red", negcol="green", main="Sequence length distribution",
-				xlab="Map length", ylab="Count", legend=c("+ve strand","-ve strand"), legendx=NULL, legendy=NULL, plot=TRUE, down=FALSE, sym.axes=TRUE, ...) {
 
+barplot.bam <- function(vdf=NULL, minlen=1, maxlen=37, col="red", axes=c(1000,-1000),main="title", ...) {
 
-	# count frequencies in the negative strand
-	neg <- .sum.bam.length(vdf, "-", minlen, maxlen)
-	
+  neg <- .sum.bam.length(vdf, "-", minlen, maxlen)
+  pos <- .sum.bam.length(vdf, "+", minlen, maxlen)
+  bavdf <- merge(pos, neg, by="length", sort=FALSE)
+  colnames(bavdf) <- c("lgth","pos","neg")
+      
+  miny <- axes[2]
+  maxy <- axes[1]
+  
+  mybreaks = seq(minlen,  maxlen, by = 2)
+  mylabels = seq(minlen,  maxlen, by = 2)
+  
+  #ggplot2
+  p1 <- ggplot() +geom_bar(data = bavdf, aes(x=lgth, y=0-neg),stat = "identity", fill=col,width=1, colour="black") 
+  p1 <- p1 +geom_bar(data = bavdf, aes(x=lgth, y=pos),stat = "identity", fill=col,width=1, colour="black") +geom_hline(yintercept=0) +geom_vline(xintercept=21.5, linetype='dotted', size=0.5)
+  p1 <- p1 +theme_classic()  +ggtitle(main) +scale_x_continuous(labels=mylabels,breaks=mybreaks)
+  p1 <- p1 +theme(axis.title.y=element_blank(),axis.title.x=element_blank(),axis.text = element_text(size = 12,colour = "black"),plot.title = element_text(face="bold", size=16,hjust = 0.5)) 
+  p1 <- p1 +ylim(c(miny,maxy))
+  p1
 
-	# count frequencies on the positive strand
-	pos <- .sum.bam.length(vdf, "+", minlen, maxlen)
-	
-	# join the two on column "length" and give the result
-	# sensible column names
-	bavdf <- merge(pos, neg, by="length", sort=FALSE)
-	colnames(bavdf) <- c("lgth","pos","neg")
-
-	# if the user wants a plot
-	if (plot==TRUE) {
-
-
-		# draw the barplot of pos and neg frequencies
-		# and with the user specified options
-		if (down==TRUE) {
-
-			# if the user wants symmetrical y-axes
-			if (sym.axes==TRUE) {
-				miny <- -1 * max(bavdf[,2:3])
-				maxy <- max(bavdf[,2:3])
-			} else {
-				# else let the data decide
-				miny <- -1 * max(bavdf[,3])
-				maxy <- max(bavdf[,2])
-			}
-
-			# plot the poistive counts
-			barplot(as.matrix(t(bavdf[,2])), beside=TRUE, names=bavdf[,1], col=c(poscol), main=main, xlab=xlab, ylab=ylab, ylim=c(miny,maxy), ...)
-
-			# add zero minus the negative counts
-			barplot(as.matrix(0-t(bavdf[,3])), beside=TRUE, col=c(negcol), add=TRUE, ...)
-		} else {
-			barplot(as.matrix(t(bavdf[,2:3])), beside=TRUE, names=bavdf[,1], col=c(poscol,negcol), main=main, xlab=xlab, ylab=ylab, ...)
-		}
-
-
-		# set defaults for legendx and legendy
-		# if no values are provided
-		if (is.null(legendx)) {
-			legendx <- 5
-		}
-
-		if (is.null(legendy)) {
-			legendy <- max(bavdf[,2:3])/2
-		}
-
-
-		# draw the legend
-		legend(legendx,legendy,legend=legend,fill=c(poscol,negcol))
-	}
-
-	# return the frequencies
-	return(bavdf)
-	
 }
 
 
