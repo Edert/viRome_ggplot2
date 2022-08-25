a5b2ba46807f1d2d4acdf9e2050c46d68281ffa3
Thomas Eder <thomas.eder@vetmeduni.ac.at>
2022-08-25 09:01:10 +0200

    Modified position.barplot to plot via ggplot2

--- R/position.barplot.R
+++ R/position.barplot.R
@@ -1,64 +1,58 @@
-position.barplot <- function(vdf=NULL, minlen=1, maxlen=37, reflen=10000, samp="", plot=TRUE, poscol="red", negcol="green") {
+position.barplot <- function(vdf=NULL, minlen=1, maxlen=37, reflen=10000, main="", col="green", axes=c(0,0)) {
+  
+  # limit the input to only contain reads
+  # of size between minlen and maxlen
+  vdf <- vdf[vdf$cliplen>=minlen&vdf$cliplen<=maxlen,]
+  
+  # get the reference name and size range as strings
+  refname = vdf$rname[1]
+  maps <- paste(minlen, "-", maxlen, sep="")
+  
+  
+  # get counts of reads at each position for 
+  # negative strand
+  npos <- .count.reads.by.position(vdf, "-")
+  
+  # get counts of reads at each position for 
+  # positive strand
+  ppos <- .count.reads.by.position(vdf, "+")
+  
+  # both of the above may "miss" positions out
+  # due to zero counts so we create a dummy
+  # data.frame with all positions in it
+  # and outer-join each of npos and ppos to it
+  # filling in resulting NS's with 0 counts
+  lgths <- data.frame(position=1:reflen)
+  
+  lpos <- merge(lgths, ppos, all.x=TRUE, sort=TRUE)
+  lneg <- merge(lgths, npos, all.x=TRUE, sort=TRUE)
+  
+  lpos[is.na(lpos)] <- 0
+  lneg[is.na(lneg)] <- 0
+  
+  # sort each of the new merged data.frames by position
+  lpos <- lpos[order(lpos$position),]
+  lneg <- lneg[order(lneg$position),]
+  
+  # calculate the range to be plotted
+  mx <- max(c(lpos[,2],lneg[,2]))
+  rng <- c(-1*mx,mx)
+  
+  if(axes[1] == 0 & axes[2] ==0){ #if not set do it automatically
+    miny <- -(mx+1)
+    maxy <- mx+1
+  }else{
+    miny <- axes[2]
+    maxy <- axes[1]
+  }
+  
+  #ggplot2
+  p1 <- ggplot() +geom_bar(data = lneg, aes(x=position, y=0-count),stat = "identity", fill=col,width=1) 
+  p1 <- p1 +geom_bar(data = lpos, aes(x=position, y=count),stat = "identity", fill=col,width=1)
+  p1 <- p1 +theme_classic() +ggtitle(main) +ylim(c(miny,maxy)) 
+  p1 <- p1 +theme(axis.title.y=element_blank(),axis.title.x=element_blank(),axis.text = element_text(size = 10,colour = "black"),plot.title = element_text(face="bold", size=16,hjust = 0.5),plot.margin = margin(10, 20, 10, 10)) 
+  p1
 
-
-	# limit the input to only contain reads
-	# of size between minlen and maxlen
-	vdf <- vdf[vdf$cliplen>=minlen&vdf$cliplen<=maxlen,]
-
-	# get the reference name and size range as strings
-	refname = vdf$rname[1]
-	maps <- paste(minlen, "-", maxlen, sep="")
-
-	
-	# get counts of reads at each position for 
-	# negative strand
-	npos <- .count.reads.by.position(vdf, "-")
-
-	# get counts of reads at each position for 
-	# positive strand
-	ppos <- .count.reads.by.position(vdf, "+")
-
-	# both of the above may "miss" positions out
-	# due to zero counts so we create a dummy
-	# data.frame with all positions in it
-	# and outer-join each of npos and ppos to it
-	# filling in resulting NS's with 0 counts
-	lgths <- data.frame(position=1:reflen)
-
-	lpos <- merge(lgths, ppos, all.x=TRUE, sort=TRUE)
-        lneg <- merge(lgths, npos, all.x=TRUE, sort=TRUE)
-      
-        lpos[is.na(lpos)] <- 0
-        lneg[is.na(lneg)] <- 0
-
-	# sort each of the new merged data.frames
-	# by position
-	lpos <- lpos[order(lpos$position),]
-	lneg <- lneg[order(lneg$position),]
-      
-	# if the users has decided to plot
-	if (plot == TRUE) {
-
-		# calculate the range to be plotted
-       		mx <- max(c(lpos[,2],lneg[,2]))
-        	rng <- c(-1*mx,mx)
-
-		# create a title for the plot
-		tit   <- paste(samp,"Distribution of",maps,"bp maps along",refname, sep=" ")
-
-		# create the barplot as counts on the positive strand and 0-counts on the negative strand
-		barplot(as.matrix(lpos[,2]), beside=TRUE, names=lpos[,1], col=c(poscol), xlab="Nucleotide position", ylab="Count", border=poscol, ylim=rng, xaxt="n", main=tit)
-        	barplot(as.matrix(0-lneg[,2]), beside=TRUE, add=TRUE, col=c(negcol),  border=negcol)
-
-		# add an axis
-        	axis(side=1)
-	}
-
-	# merge the positive and negative count data.frames
-	# and give them useful column names and return to the user
-	ret <- merge(lpos, lneg, by="position", sort=FALSE)
-	colnames(ret) <- c("position","poscount","negcount")
-	return(ret)
 }
 
 
